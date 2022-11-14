387385344637a677f172468b65cc578c8e30a31d
Harinathlee <harinathlee2598@gmail.com>
2022-11-14 22:54:16 +0400

    a

--- src/About/TopBar.jsx
+++ src/About/TopBar.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Topbar.scss';
-import logo from "../logo192.png"
+import "./Topbar.scss";
+import logo from "../logo192.png";
 const TopBar = () => {
   return (
     <div className="nav-main">
       <nav className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="https://github.com/Harinathlee/upoint-query-builder">
-       
-          <h2 className="main-title"> <img src={logo} alt='logo' className='logo'></img>Upoint-Query-Replicator</h2> 
+        <a
+          className="navbar-brand"
+          href="https://github.com/Harinathlee/upoint-query-builder"
+        >
+          <h2 className="main-title">
+            <img src={logo} alt="logo" className="logo"></img>
+            Upoint-Query-Replicator
+          </h2>
         </a>
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <Link to="/AboutMe" classNameName="">
-              <p>AboutMe</p>
+              <p className="link-names">AboutMe</p>
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/Tutorial" classNameName="">
-              <p>Tutorial</p>
+              <p className="link-names">Tutorial</p>
             </Link>
           </li>
         </ul>
